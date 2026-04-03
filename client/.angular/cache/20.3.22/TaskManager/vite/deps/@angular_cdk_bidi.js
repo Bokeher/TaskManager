47ed15fc5940956e82e5beb76eb64834fc610eed
Bokeher <bokeher47@gmail.com>
2026-04-03 16:14:57 +0200

    refactor(client): rename Frontend to client

---
+++ client/.angular/cache/20.3.22/TaskManager/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,17 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-BAA3XK3I.js";
+import "./chunk-MX56E4ZR.js";
+import "./chunk-JRFR6BLO.js";
+import "./chunk-HWYXSU2G.js";
+import "./chunk-MARUHEWW.js";
+import "./chunk-EIB7IA3J.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
