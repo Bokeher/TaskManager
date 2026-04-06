ad156ff0d64bf81e4080a193596bbd44270fdc8c
Bokeher <bokeher47@gmail.com>
2026-04-06 10:56:21 +0200

    feat(client): set home as default route and add fallback redirect

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectComponent } from './project/project.component';
-import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'project', component: ProjectComponent },
-  { path: 'home', component: AppComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  
+  { path: '**', redirectTo: ''},
 ];
 
 @NgModule({
